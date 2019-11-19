1155bd5e98ae27def618127237d4928ff064fbf0
goncalogomes <goncalogomes@WIN-GESFLHLR4EE>
2019-11-19 16:04:01 +0000

    1º push

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        diasemana semana = new diasemana();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            semana.receber = textBox1.Text;
+            label2.Text = semana.envia;
+        }
+    }
+}
---
+++ WindowsFormsApp1/diasemana.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    
+    class diasemana
+    {
+        private DateTime data;
+
+        public string receber
+        {
+            set
+            {
+                data = Convert.ToDateTime(value);
+            }
+        }
+
+        public string envia
+        {
+            get
+            {
+                return data.ToString("ddddd");
+            }
+        }
+    }
+}
